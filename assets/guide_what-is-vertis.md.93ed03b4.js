f12cb6fd04f1df1387ca13ce6a685691500da893
cadgerfeast <cadgerfeast@users.noreply.github.com>
2023-10-05 14:49:55 +0000

    deploy: cffb8273641734cc3e9704c4bd230b719fc3ba94

--- assets/guide_what-is-vertis.md.93ed03b4.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,o as t,c as a}from"./chunks/framework.a7175731.js";const m=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"guide/what-is-vertis.md","filePath":"guide/what-is-vertis.md"}'),s={name:"guide/what-is-vertis.md"};function r(i,o,c,n,d,p){return t(),a("div")}const f=e(s,[["render",r]]);export{m as __pageData,f as default};
